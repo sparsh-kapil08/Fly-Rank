65d56b1cd51b9d2ee2a322d6a3449b226b01f32f
sparsh-kapil08 <sparshkapil2008@gmail.com>
2026-07-15 02:05:29 +0530

    Stage 5: Swagger UI

--- Backend-Track/Assgn2/app.js
+++ Backend-Track/Assgn2/app.js
@@ -1,7 +1,10 @@
 const express=require("express");
+const swaggerUi=require("swagger-ui-express");
+const swaggerDocument=require("./openapi.json");
 const app=express();
 const PORT=3000;
 app.use(express.json());
+app.use("/docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 let tasks=[{
     id:1,
     title:"Task 1",
