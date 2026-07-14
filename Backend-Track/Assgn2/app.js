82d999b64492ef07e4ce18b8bf99d5325fd3dfeb
sparsh-kapil08 <sparshkapil2008@gmail.com>
2026-07-15 00:59:16 +0530

    Stage 0: hello server

--- Backend-Track/Assgn2/app.js
+++ Backend-Track/Assgn2/app.js
@@ -2,12 +2,11 @@ const express=require("express");
 const app=express();
 const PORT=4000;
 app.get("/",(req,res)=>{
+    res.status(200);
     res.send("THE MAIN PAGE");
 
 });
-app.get("/pg2",(req,res)=>{
-    res.send("THE SECOND PAGE");
-});
+
 app.listen(PORT,err=>{
     console.log(`server is running on port ${PORT}`);
     if(err){
