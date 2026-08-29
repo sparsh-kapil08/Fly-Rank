045fc3f653fab66d7cefc4998874b3ceff8c5d18
sparsh-kapil08 <sparshkapil2008@gmail.com>
2026-08-29 13:41:08 +0530

    Stage 1: signup and login routes working

--- Backend-Track/Assgn5/server.js
+++ Backend-Track/Assgn5/server.js
@@ -2,7 +2,43 @@ const client = require("./db.js");
 const express = require("express");
 const app=express();
 const PORT=process.env.PORT || 3000;
-
+app.use(express.json());
+app.post("/auth/signup",async (req,res)=>{
+    if(!req.body.email || !req.body.password){
+        res.status(400);
+        res.json({"error":"The Server never trusts the client"});
+    }
+    console.log(req.body.email);
+    const{data,error}=await client.auth.signUp({
+        email:req.body.email,
+        password:req.body.password
+    })
+    if(error){
+        res.status(400);
+        res.json({"error":error.message});
+    }
+    else{
+        res.status(200);
+        res.json({"message":"Created"});
+    }
+});
+app.post("/auth/login",async (req,res)=>{
+    if(!req.body.email || !req.body.password){
+        res.status(400);
+        res.json({"error":"The Server never trusts the client"});
+    }
+    const{data,error}=await client.auth.signInWithPassword({
+        email:req.body.email,
+        password:req.body.password
+    })
+    if(error){
+        res.status(401);
+        res.json({"error":"Invalid Login Credentials"});
+    }else{
+        res.status(200);
+        res.json({"token":data.session.access_token,"refresh_token":data.session.refresh_token});
+    }
+})
 app.listen(PORT, () => {
   console.log(`Server running and connected to supabase`);
 });
